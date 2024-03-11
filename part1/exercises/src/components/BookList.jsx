1d537dd649121c07806cd8b82ac78daada0c194b
unknown <150742495+Tinxte@users.noreply.github.com>
2024-03-10 19:16:55 -0500

    React Part 1 exercises

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book List";
+   let book1 = "https://upload.wikimedia.org/wikipedia/commons/c/c3/Phelsuma_l._laticauda.jpg";
+   let book2 = "https://upload.wikimedia.org/wikipedia/commons/a/a1/Thorny_Devil_crop.jpg";
+   let book3 = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Juvenile_bearded_dragon.jpg/800px-Juvenile_bearded_dragon.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1}  height= "400px" alt="Geckos for Dummies" />
+         <img src={book2}  height= "400px" alt="Thorny Devils for Dummies" />
+         <img src={book3}  height= "400px" alt="Bearded Dragons for Dummies" />
       </div>      
    );
 }
---
+++ part1/exercises/src/components/HobbyLinks.jsx
@@ -0,0 +1,9 @@
+function HobbyLinks() {
+    let hobbyLinks = ["launchcode.org", "google.com"];
+    return (
+    <div>    
+         <a href= {hobbyLinks[0]}> Link text </a>
+         <a href= {hobbyLinks[1]}> Link text </a>
+    </div> 
+    )
+}
