532259a59654a72a81e058c24c8a2d509d514f82
unknown <150742495+Tinxte@users.noreply.github.com>
2024-03-14 16:26:58 -0500

    part 2 exercises for React

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,12 +1,12 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json'
 
 
 function Profile()
 {
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
          <Button />
       </>  
    );
